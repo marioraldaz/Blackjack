f05930604e3c72e6312f500f619dee7afd435eac
marioraldaz <130656068+marioraldaz@users.noreply.github.com>
2023-06-08 11:35:45 +0200

    restart game function added, appearance changes

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ let points=0;
 let cardEl = document.getElementById("cards");
 let pointsEl = document.getElementById("points");
 let msgEl = document.getElementById("msg");
-
+let startPanelEl = document.getElementById("start-panel");
 let started=false
-
 
 function getCard(){
     let card = Math.floor(Math.random()*10)+2;
@@ -22,11 +21,14 @@ function addCard(){
     } else if(points===21){
         msgEl.textContent = "Blackjack!"
     } else{
+        startPanelEl.textContent="Press start to play again."
         msgEl.textContent = "You lost!"
     }
     }
 }
 function startGame() {
+    points=0
+    cardEl.textContent = ""
     let firstCard=getCard()
     let secondCard=getCard()
     points =firstCard+secondCard
@@ -34,4 +36,5 @@ function startGame() {
     cardEl.textContent+= firstCard+" "+secondCard
     pointsEl.textContent= points
     msgEl.textContent = "Do you want to try and get closer?"
+    startPanelEl.textContent= "Good luck!"
 }
